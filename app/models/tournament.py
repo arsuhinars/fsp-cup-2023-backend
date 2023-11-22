a83530be96f4b1d0a7d947f173fc2d79d5fee525
Sedo-KFM <fedor122448@gmail.com>
2023-11-22 15:30:50 +0400

    Update tournaments

--- app/models/tournament.py
+++ app/models/tournament.py
@@ -1,9 +1,10 @@
 from datetime import date
 
-from sqlalchemy import Date, ForeignKey, String
+from sqlalchemy import ForeignKey, String, Date, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.db import Base
+from app.schemas.tournament_schema import TournamentStateEnum
 
 
 class Tournament(Base):
@@ -14,10 +15,11 @@ class Tournament(Base):
     location: Mapped[str] = mapped_column(String(50))
     discipline: Mapped[str] = mapped_column(String(50))
     date_registration: Mapped[date] = mapped_column(Date)
-    date_start: Mapped[date] = mapped_column(Date)
+    date_begin: Mapped[date] = mapped_column(Date)
     date_end: Mapped[date] = mapped_column(Date)
-    date_award: Mapped[date] = mapped_column(Date)
-    state: Mapped[str] = mapped_column(String(50))
+    date_awards: Mapped[date] = mapped_column(Date)
     main_judge_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    state: Mapped[TournamentStateEnum] = mapped_column(Enum(TournamentStateEnum))
 
     tournament_sets: Mapped[list["TournamentSet"]] = relationship()
+    main_judge: Mapped["User"] = relationship(back_populates="judge_tournaments")
--- app/schemas/tournament_schema.py
+++ app/schemas/tournament_schema.py
@@ -1,9 +1,16 @@
 from datetime import date
+from enum import StrEnum
 
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
 
-from app.models.tournament import Tournament
+
+class TournamentStateEnum(StrEnum):
+    JUST_CREATED = "JUST_CREATED"
+    REGISTRATION_OPENED = "REGISTRATION_OPENED"
+    REGISTRATION_CLOSED = "REGISTRATION_CLOSED"
+    ONGOING = "ONGOING"
+    FINISHED = "FINISHED"
 
 
 class TournamentSchema(BaseModel):
@@ -12,7 +19,12 @@ class TournamentSchema(BaseModel):
     location: Annotated[str, Field(max_length=50, examples=["Location"])]
     discipline: Annotated[str, Field(max_length=50, examples=["Discipline"])]
     date_registration: Annotated[date, Field(examples=["2000-01-01"])]
-    date_start: Annotated[date, Field(examples=["2000-01-01"])]
-    date_end: Annotated[date, Field(examples=["2000-01-01"])]
-    date_award: Annotated[date, Field(examples=["2000-01-01"])]
-    state: Annotated[str, Field(max_length=50, examples=["State"])]
+    date_begin: Annotated[date, Field(examples=["2000-02-01"])]
+    date_end: Annotated[date, Field(examples=["2000-03-01"])]
+    date_awards: Annotated[date, Field(examples=["2000-04-01"])]
+    main_judge_id: Annotated[int, Field(example=1)]
+    state: Annotated[TournamentStateEnum, Field(examples=["JUST_CREATED",
+                                                          "REGISTRATION_OPENED",
+                                                          "REGISTRATION_CLOSED",
+                                                          "ONGOING",
+                                                          "FINISHED"])]
