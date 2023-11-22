d85d12bb632ea3bf110b861d473b012cdb4f9390
Sedo-KFM <fedor122448@gmail.com>
2023-11-22 18:41:45 +0400

    Fix TeamCreateSchema

--- app/schemas/team_create_schema.py
+++ app/schemas/team_create_schema.py
@@ -5,7 +5,6 @@ from app.models.team import Team
 
 
 class TeamCreateSchema(BaseModel):
-    id: Annotated[int, Field(examples=[1])]
     leader_id: Annotated[int, Field(examples=[1])]
     name: Annotated[str, Field(max_length=50, examples=["Name"])]
 
