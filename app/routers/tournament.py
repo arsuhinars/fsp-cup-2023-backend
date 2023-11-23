e251346bc7651924a46fcd060d97ceb019aeacec
arsuhinars <arsuhinars@gmail.com>
2023-11-23 13:10:47 +0400

    Added tournament router

--- app/routers/tournament.py
+++ app/routers/tournament.py
@@ -1,51 +1,101 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 
 from app.schemas.team_composition_schema import TeamCompositionSchema
-from app.schemas.tournament_create_schema import TournamentCreateSchema
-from app.schemas.tournament_schema import TournamentSchema
-from app.schemas.tournament_update_schema import TournamentUpdateSchema
+from app.schemas.tournament_request_schema import TournamentRequestSchema
+from app.schemas.tournament_schema import (
+    TournamentCreateSchema,
+    TournamentSchema,
+    TournamentUpdateSchema,
+)
+from app.schemas.user_schema import UserSchema
+from app.security import authenticate, require_judge, require_team_captain
 from app.services import tournament_service
 
 router = APIRouter(prefix="/tournaments", tags=["Tournament"])
 
 
-@router.post("/")
-def create_tournament(tournament: TournamentCreateSchema) -> TournamentSchema:
-    return tournament_service.create(tournament)
+@router.post("/", response_model=TournamentSchema)
+def create_tournament(
+    tournament: TournamentCreateSchema,
+    user: Annotated[UserSchema, Depends(require_judge)],
+) -> TournamentSchema:
+    return tournament_service.create(tournament, user.id)
 
 
-@router.post("/{tournament_id}/set")
-def set_tournament_set(tournament_id: int, team_ids: list[int]) -> list[int]:
-    return tournament_service.set_team_comps(tournament_id, team_ids)
-
-
-@router.get("/")
+@router.get(
+    "/", response_model=list[TournamentSchema], dependencies=[Depends(authenticate)]
+)
 def get_all_tournaments():
     return tournament_service.get_all()
 
 
-@router.get("/{tournament_id}")
-def get_tournament(tournament_id: int):
+@router.get(
+    "/{tournament_id}",
+    response_model=TournamentSchema,
+    dependencies=[Depends(authenticate)],
+)
+def get_tournament_by_id(tournament_id: int):
     return tournament_service.get_by_id(tournament_id)
-
-
-@router.get("/{tournament_id}/set")
-def get_tournament_team_comps(tournament_id: int) -> list[TeamCompositionSchema]:
-    return list(
-        map(
-            lambda team_comp: TeamCompositionSchema.model_validate(team_comp),
-            tournament_service.get_team_comps(tournament_id),
-        )
-    )
 
 
 @router.put("/{tournament_id}")
 def update_tournament(
-    tournament_id: int, tournament: TournamentUpdateSchema
+    tournament_id: int,
+    tournament: TournamentUpdateSchema,
+    user: Annotated[UserSchema, Depends(require_judge)],
 ) -> TournamentSchema:
+    # TODO проверка на судью
     return tournament_service.update(tournament_id, tournament)
 
 
 @router.delete("/{tournament_id}")
 def delete_tournament(tournament_id: int) -> bool:
     return tournament_service.delete(tournament_id)
+
+
+@router.get(
+    "/{tournament_id}/team_compositions",
+    response_model=list[TeamCompositionSchema],
+    dependencies=[Depends(authenticate)],
+)
+def get_tournament_team_compositions(tournament_id: int):
+    ...
+
+
+@router.get("/{tournament_id}/requests", response_model=list[TournamentRequestSchema])
+def get_tournament_requests(
+    tournament_id: int,
+    user: Annotated[UserSchema, Depends(require_judge)],
+):
+    ...
+
+
+@router.get("/{tournament_id}/requests/my", response_model=TournamentRequestSchema)
+def get_my_tournament_request(
+    tournament_id: int,
+    user: Annotated[UserSchema, Depends(require_team_captain)],
+):
+    ...
+
+
+@router.post("/{tournament_id}/requests", response_model=TournamentRequestSchema)
+def create_tournament_request(
+    user: Annotated[UserSchema, Depends(require_team_captain)]
+):
+    ...
+
+
+@router.post("/requests/{request_id}/accept")
+def accept_tournament_request(
+    request_id: int, user: Annotated[UserSchema, Depends(require_judge)]
+):
+    ...
+
+
+@router.post("/requests/{request_id}/decline")
+def decline_tournament_request(
+    request_id: int, user: Annotated[UserSchema, Depends(require_judge)]
+):
+    ...
