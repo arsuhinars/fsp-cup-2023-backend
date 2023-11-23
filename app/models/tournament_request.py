53eb6cc4e2166f83501937d21dcafda1ce32d51d
Sedo-KFM <fedor122448@gmail.com>
2023-11-24 02:29:35 +0400

    Add tournament requests

--- app/models/tournament_request.py
+++ app/models/tournament_request.py
@@ -1,6 +1,26 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.core.db import Base
+from app.schemas.tournament_request_schema import TournamentRequestStatus
 
 
 class TournamentRequest(Base):
-    # TODO: реализовать поля
-    ...
+    __tablename__ = "tournament_requests"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tournament_id: Mapped[int] = mapped_column(ForeignKey("tournament.id"))
+    team_composition_id: Mapped[int] = mapped_column(ForeignKey("team_composition.id"))
+    status: Mapped[TournamentRequestStatus] = mapped_column(default=TournamentRequestStatus.PENDING)
+
+    team_composition: Mapped["TeamComposition"] = relationship(
+        foreign_keys=[team_composition_id]
+    )
+    tournament: Mapped["Tournament"] = relationship(
+        foreign_keys=[tournament_id]
+    )
+
+    @hybrid_property
+    def active_players(self):
+        return self.team_composition.players
--- app/services/tournament_requests_service.py
+++ app/services/tournament_requests_service.py
@@ -1,21 +1,41 @@
-from app.schemas.tournament_request_schema import TournamentRequestSchema
+from app.core import db
+from app.models.tournament_request import TournamentRequest
+from app.schemas.tournament_request_schema import TournamentRequestSchema, TournamentRequestStatus
+import app.repositories.tournament_requests_repository as tournament_request_repo
 
 
-def create_request(tournament_id: int, team_id: int) -> TournamentRequestSchema:
-    ...
+def create_request(tournament_id: int, team_comp_id: int) -> TournamentRequestSchema:
+    with db.create_session() as session:
+        request = tournament_request_repo.save(session, TournamentRequest(
+            tournament_id=tournament_id,
+            team_composition_id=team_comp_id
+        ))
+        return TournamentRequestSchema.model_validate(request)
 
 
 def get_tournament_requests(tournament_id: int) -> list[TournamentRequestSchema]:
-    ...
+    with db.create_session() as session:
+        requests = tournament_request_repo.get_by_tournament_id(session, tournament_id)
+        return [TournamentRequestSchema.model_validate(request) for request in requests]
 
 
-def get_by_team_id(tournament_id: int, team_id: int) -> TournamentRequestSchema:
-    ...
+def get_by_team_comp_id(tournament_id: int, team_comp_id: int) -> TournamentRequestSchema:
+    with db.create_session() as session:
+        request = tournament_request_repo.get_by_tournament_id_and_team_comp_id(session, tournament_id, team_comp_id)
+        return TournamentRequestSchema.model_validate(request)
 
 
 def accept_request(request_id: int) -> TournamentRequestSchema:
-    ...
+    with db.create_session() as session:
+        request = tournament_request_repo.get_by_id(session, request_id)
+        request.status = TournamentRequestStatus.ACCEPTED
+        request = tournament_request_repo.save(session, request)
+        return TournamentRequestSchema.model_validate(request)
 
 
 def decline_request(request_id: int) -> TournamentRequestSchema:
-    ...
+    with db.create_session() as session:
+        request = tournament_request_repo.get_by_id(session, request_id)
+        request.status = TournamentRequestStatus.DECLINED
+        request = tournament_request_repo.save(session, request)
+        return TournamentRequestSchema.model_validate(request)
