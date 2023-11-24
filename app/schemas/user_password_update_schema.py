36d9b44e8ea8ee3487e3b6e44039e6e1933141a3
Sedo-KFM <fedor122448@gmail.com>
2023-11-24 16:54:19 +0400

    Remove deprecated schemas

--- app/schemas/user_password_update_schema.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-from pydantic import BaseModel, Field
-
-
-class UserPasswordUpdateSchema(BaseModel):
-    new_password: Annotated[
-        str, Field(min_length=8, max_length=50, examples=["password"])
-    ]
